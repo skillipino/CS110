5a51d089a3f8df31c24cfab88d3a70c496c72c0f
Dan <104574085+skillipino@users.noreply.github.com>
2023-10-09 13:06:52 -0400

    Add files via upload

--- ramanujan_numbers.py
+++ ramanujan_numbers.py
@@ -11,38 +11,24 @@
 import stdio
 import sys
 
+#assigning command line arg to variable
 n = int(sys.argv[1])
 a = 1
-b = 12
-c = 2
-d = 11
 
-#while 1 <= a*a*a <= n:
-
-   # while a*a*a + 1 <= b*b*b <= n-a*a*a:
-
-        #while a*a*a + 1 <= c*c*c <= n:
-
-           # while c*c*c + 1 <= d*d*d <= n-c*c*c:
-for i in range(1729, n+1):
-    b = int(i ** (1 / 3))
-    a = int((n - b * b * b) ** (1 / 3))
-    c = a + 1
-    d = b
-    print(1)
-    while (c*c*c + d*d*d != i):
-        print(2)
-        for j in range(c,d):
-
-            if j*j*j + d*d*d == i:
-                c = j
-                break
-            else:
-                d -= 1
-        if a * a * a + b * b * b == c * c * c + d * d * d:
-            total = (a * a * a + b * b * b)
-            stdio.writeln(
-                str(total) + " = " + str(a) + "^3" + " + " + str(b) + "^3" + " = " + str(c) + "^3" + " + " + str(d) + "^3")
-
-
-
+#nested while loops
+while 1 <= a*a*a <= n:
+    b = a + 1
+    while a*a*a + 1 <= b*b*b <= n-a*a*a:
+        c = a + 1
+        while a*a*a + 1 <= c*c*c <= n:
+            d = c + 1
+            while c*c*c + 1 <= d*d*d <= n-c*c*c:
+                x = a*a*a + b*b*b
+                y = c*c*c + d*d*d
+                if x==y: #checks that the sum of both factors are equal
+                    total = (a * a * a + b * b * b)
+                    stdio.writeln(str(total) + " = " + str(a) + "^3" + " + " + str(b) + "^3" + " = " + str(c) + "^3" + " + " + str(d) + "^3")
+                d += 1
+            c += 1
+        b += 1
+    a += 1
